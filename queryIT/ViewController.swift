dc6bb420580f5b687d6b7d262bc04ad699416286
VictorSantoryu <victor.santoryu@github.com>
2019-04-03 21:07:46 -0300

    Initial Commit

---
+++ queryIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  queryIT
+//
+//  Created by Victor on 03/04/2019.
+//  Copyright © 2019 Rinver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

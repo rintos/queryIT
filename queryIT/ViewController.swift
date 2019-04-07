cbcd4275f30531d8df30030bb69ae5b02fa75745
VictorSantoryu <victor.santoryu@github.com>
2019-04-07 14:01:08 -0300

    Listando 2 campos do FireBase para TableView

--- queryIT/ViewController.swift
+++ queryIT/ViewController.swift
@@ -7,13 +7,75 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var referencia: DatabaseReference?
+    var databaseHandle: DatabaseHandle?
+    
+    var listaFirebase = [String]()
+
+    
+    var lista = [ "Endereco IP", "Atalho selecionar"]
+    
+    
+//    var lista = [(titulo: "Endereco IP", texto: "Digite o comando ifconfig no terminal e tecle enter"),
+//               (titulo: "Atalho selecionar", texto: "shift + optional seta para cima ou para baixo")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        referencia = Database.database().reference()
+        
+        databaseHandle = referencia?.child("contexto").observe(.childAdded, with: { (dados) in
+
+            let contexto = dados.value as? NSDictionary
+
+            if let meuContexto = contexto {
+                let tituloFirebase = meuContexto["titulo"] as? String ?? ""
+                self.listaFirebase.append(tituloFirebase)
+                self.tableView.reloadData()
+
+            }
+
+
+        })
+
+          print("teste de array: \(listaFirebase)")
+
+        let dadosTitulo = referencia?.child("contexto").child("003").child("titulo")
+        let dadosTexto = referencia?.child("contexto").child("003").child("texto")
+        dadosTexto?.setValue("3Tesxto teste titulo 203")
+        dadosTitulo?.setValue("3titulo teste 03")
+        
+
+
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaFirebase.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celulaID", for: indexPath)
+        let linha = indexPath.row
+        celula.textLabel?.text = listaFirebase[linha]
+        celula.detailTextLabel?.text = listaFirebase[linha]
+
+        
+        
+        return celula
+    }
+    
+    
 
 
 }
